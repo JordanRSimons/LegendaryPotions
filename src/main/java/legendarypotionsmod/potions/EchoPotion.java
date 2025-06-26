1d3f434e75a0b8e6a7406134bc727fbf8a2c7e62
JordanRSimons <jords314@gmail.com>
2025-06-25 23:43:04 -0500

    Bottled Echo Form complete?

--- src/main/java/legendarypotionsmod/potions/EchoPotion.java
+++ src/main/java/legendarypotionsmod/potions/EchoPotion.java
@@ -1,7 +1,13 @@
 package legendarypotionsmod.potions;
 
 import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.blue.EchoForm;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.CardHelper;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import static legendarypotionsmod.BasicMod.makeID;
 
@@ -16,4 +22,23 @@ public class EchoPotion extends BasePotion {
         super(ID, 1, PotionRarity.PLACEHOLDER, PotionSize.MOON, LIQUID_COLOR, HYBRID_COLOR, SPOTS_COLOR);
     }
 
+    @Override
+    public String getDescription() {
+        if (potency == 1) {
+            return String.format(DESCRIPTIONS[0], potency) + DESCRIPTIONS[1];
+        } else {
+            return String.format(DESCRIPTIONS[0], potency) + DESCRIPTIONS[2];
+        }
+    }
+
+    @Override
+    public void use(AbstractCreature abstractCreature) {
+        if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+            for (int i = 0; i < potency; i++) {
+                AbstractCard echoForm = new EchoForm().makeCopy();
+                echoForm.setCostForTurn(0);
+                addToBot(new MakeTempCardInHandAction(echoForm, 1, false));
+            }
+        }
+    }
 }
