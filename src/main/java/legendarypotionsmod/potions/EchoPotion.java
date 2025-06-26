1e6e61c0121e2eabe62448815664bfdfc82b92bb
JordanRSimons <jords314@gmail.com>
2025-06-25 22:54:00 -0500

    Setting up to create first potion, Bottled Echo Form - Fixed typos and added potion info.

--- src/main/java/legendarypotionsmod/potions/EchoPotion.java
+++ src/main/java/legendarypotionsmod/potions/EchoPotion.java
@@ -5,14 +5,15 @@ import com.megacrit.cardcrawl.helpers.CardHelper;
 
 import static legendarypotionsmod.BasicMod.makeID;
 
-public class EchoPotion {
-}
-
-public class MyPotion extends BasePotion {
-    public static final String ID = makeID(MyPotion.class.getSimpleName());
+public class EchoPotion extends BasePotion {
+    public static final String ID = makeID(EchoPotion.class.getSimpleName());
 
     private static final Color LIQUID_COLOR = CardHelper.getColor(255, 0, 255);
     private static final Color HYBRID_COLOR = CardHelper.getColor(255, 0, 255);
     private static final Color SPOTS_COLOR = CardHelper.getColor(255, 0, 255);
+
+    public EchoPotion() {
+        super(ID, 1, PotionRarity.PLACEHOLDER, PotionSize.MOON, LIQUID_COLOR, HYBRID_COLOR, SPOTS_COLOR);
+    }
 
 }
