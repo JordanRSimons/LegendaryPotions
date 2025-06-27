35a5948f29061a3367cf94154b7a81fe74f05e06
JordanRSimons <jords314@gmail.com>
2025-06-27 01:02:24 -0500

    ReplacePotionsUC glitched, adds full number of potions no matter what.

--- src/main/java/legendarypotionsmod/relics/ReplacePotionsUC.java
+++ src/main/java/legendarypotionsmod/relics/ReplacePotionsUC.java
@@ -5,12 +5,14 @@ import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import legendarypotionsmod.potions.LegendaryPotionPool;
 
+import java.util.ArrayList;
+
 import static legendarypotionsmod.legendarypotions.makeID;
 
 public class ReplacePotionsUC extends BaseRelic {
     private static final String NAME = "ReplacePotionsUC"; //The name will be used for determining the image file as well as the ID.
     public static final String ID = makeID(NAME); //This adds the mod's prefix to the relic ID, resulting in modID:MyRelic
-    private static final AbstractRelic.RelicTier RARITY = AbstractRelic.RelicTier.BOSS; //The relic's rarity.
+    private static final AbstractRelic.RelicTier RARITY = RelicTier.UNCOMMON; //The relic's rarity.
     private static final AbstractRelic.LandingSound SOUND = AbstractRelic.LandingSound.CLINK; //The sound played when the relic is clicked.
 
     public ReplacePotionsUC() {
@@ -19,27 +21,50 @@ public class ReplacePotionsUC extends BaseRelic {
 
     @Override
     public void onEquip() {
-        int filledSlots = 0;
+        ArrayList<AbstractPotion> potionsToReplace = new ArrayList<>();
 
-        // Count how many potion slots are currently occupied
-        for (AbstractPotion potion : AbstractDungeon.player.potions) {
-            if (potion != null && potion.isObtained) {
-                filledSlots++;
+        // Debug: show potion info per slot
+        System.out.println("[ReplacePotionsUC] Checking current potions:");
+
+        for (int i = 0; i < AbstractDungeon.player.potionSlots; i++) {
+            AbstractPotion potion = AbstractDungeon.player.potions.get(i);
+
+            if (potion == null) {
+                System.out.println("  Slot " + i + ": null");
+            } else {
+                System.out.println("  Slot " + i + ": " + potion.ID + ", isObtained=" + potion.isObtained);
+            }
+
+            // Only add potions that are obtained and not empty placeholder
+            if (potion != null && potion.isObtained && !potion.ID.equals("Empty")) {
+                potionsToReplace.add(potion);
             }
         }
 
-        // Remove all potions
-        AbstractDungeon.player.potions.clear();
+        System.out.println("[ReplacePotionsUC] Removing " + potionsToReplace.size() + " potions and replacing with legendary potions.");
 
-        // Refill only the previously filled slots with random legendary potions
-        for (int i = 0; i < filledSlots; i++) {
+        // Remove all potions to be replaced
+        // Remove all potions to be replaced
+        for (AbstractPotion potion : potionsToReplace) {
+            AbstractDungeon.player.removePotion(potion);
+        }
+
+        // Add legendary potions equal to how many were removed
+        for (int i = 0; i < potionsToReplace.size(); i++) {
             try {
                 Class<? extends AbstractPotion> potionClass = LegendaryPotionPool.getRandomLegendaryPotion();
-                AbstractPotion potion = potionClass.getDeclaredConstructor().newInstance();
-                AbstractDungeon.player.obtainPotion(i, potion);
+                AbstractPotion newPotion = potionClass.getDeclaredConstructor().newInstance();
+                AbstractDungeon.player.obtainPotion(newPotion);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+
 }
