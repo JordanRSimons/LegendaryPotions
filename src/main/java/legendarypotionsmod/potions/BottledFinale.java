5ec7fc4e530632acfe8ebdcbb37486183e25f5c6
JordanRSimons <jords314@gmail.com>
2025-06-28 00:49:53 -0500

    Essence of the End polished and colored.

--- src/main/java/legendarypotionsmod/potions/BottledFinale.java
+++ src/main/java/legendarypotionsmod/potions/BottledFinale.java
@@ -2,25 +2,28 @@ package legendarypotionsmod.potions;
 
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.green.GrandFinale;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.CardHelper;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import static legendarypotionsmod.legendarypotions.makeID;
 
 public class BottledFinale extends BasePotion {
     public static final String ID = makeID(BottledFinale.class.getSimpleName());
 
-    private static final Color LIQUID_COLOR = CardHelper.getColor(58, 223, 232); // light blue
-    private static final Color HYBRID_COLOR = CardHelper.getColor(24, 120, 125); // darker blue
+    private static final Color LIQUID_COLOR = CardHelper.getColor(161, 27, 32); // blood red
+    private static final Color HYBRID_COLOR = CardHelper.getColor(46, 5, 6); // nearly black
     private static final Color SPOTS_COLOR = null;
 
     public BottledFinale() {
-        super(ID, 1, PotionRarity.PLACEHOLDER, PotionSize.T, LIQUID_COLOR, HYBRID_COLOR, SPOTS_COLOR);
+        super(ID, 1, PotionRarity.PLACEHOLDER, PotionSize.SPIKY, LIQUID_COLOR, HYBRID_COLOR, SPOTS_COLOR);
     }
 
     @Override
@@ -40,16 +43,21 @@ public class BottledFinale extends BasePotion {
 
     @Override
     public void use(AbstractCreature target) {
-        AbstractPlayer p = AbstractDungeon.player;
+        if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+            AbstractPlayer p = AbstractDungeon.player;
 
-        // Add potency copies of Grand Finale to your hand (unupgraded)
-        for (int i = 0; i < potency; i++) {
-            AbstractCard card = new GrandFinale();
-            p.hand.addToHand(card);
+            // Add potency copies of Grand Finale to your hand (unupgraded)
+            for (int i = 0; i < potency; i++) {
+                AbstractCard card = new GrandFinale();
+                addToBot(new MakeTempCardInHandAction(card, 1));
+            }
+
+            // Delay before scry+draw
+            addToBot(new WaitAction(0.4F)); // 0.4 seconds delay (adjustable)
+
+            // Scry then draw
+            addToBot(new ScryAction(10 * potency));
+            addToBot(new DrawCardAction(p, 2 * potency));
         }
-
-        // Scry then draw
-        addToBot(new ScryAction(10 * potency));
-        addToBot(new DrawCardAction(p, 2 * potency));
     }
 }
