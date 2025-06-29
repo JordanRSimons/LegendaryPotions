58113db0d61a781070d81af13399aa4d4f57c59f
JordanRSimons <jords314@gmail.com>
2025-06-29 14:19:11 -0500

    Entropy Pot works inside combat, fails outside combat.

--- src/main/java/legendarypotionsmod/potions/BottledEntropy.java
+++ src/main/java/legendarypotionsmod/potions/BottledEntropy.java
@@ -2,6 +2,7 @@ package legendarypotionsmod.potions;
 
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ObtainPotionAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
@@ -9,6 +10,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.potions.EntropicBrew;
 import com.megacrit.cardcrawl.potions.PotionSlot;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.vfx.ObtainPotionEffect;
 
 
@@ -54,10 +56,53 @@ public class BottledEntropy extends BasePotion {
         int maxSlots = 5;
         int slotsToAdd = Math.min(potency, maxSlots - p.potionSlots);
 
-        if (slotsToAdd > 0) {
-            p.potionSlots += slotsToAdd;
-            for (int i = 0; i < slotsToAdd; i++) {
-                p.potions.add(new PotionSlot(p.potions.size()));
+        if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+            if (slotsToAdd > 0) {
+                addToBot(new AbstractGameAction() {
+                    @Override
+                    public void update() {
+                        p.potionSlots += slotsToAdd;
+                        for (int i = 0; i < slotsToAdd; i++) {
+                            p.potions.add(new PotionSlot(p.potions.size()));
+                        }
+                        isDone = true;
+                    }
+                });
+            }
+
+            addToBot(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    if (p.hasRelic("Sozu")) {
+                        p.getRelic("Sozu").flash();
+                        isDone = true;
+                        return;
+                    }
+                    for (int i = 0; i < p.potionSlots; i++) {
+                        if (p.potions.get(i) instanceof PotionSlot) {
+                            addToBot(new ObtainPotionAction(AbstractDungeon.returnRandomPotion(true)));
+                        }
+                    }
+                    isDone = true;
+                }
+            });
+        } else {
+            // Outside combat: add slots immediately (since no action queue)
+            if (slotsToAdd > 0) {
+                p.potionSlots += slotsToAdd;
+                for (int i = 0; i < slotsToAdd; i++) {
+                    p.potions.add(new PotionSlot(p.potions.size()));
+                }
+            }
+
+            if (p.hasRelic("Sozu")) {
+                p.getRelic("Sozu").flash();
+            } else {
+                for (int i = 0; i < p.potionSlots; i++) {
+                    if (p.potions.get(i) instanceof PotionSlot) {
+                        AbstractDungeon.effectsQueue.add(new ObtainPotionEffect(AbstractDungeon.returnRandomPotion()));
+                    }
+                }
             }
         }
 
@@ -69,20 +114,6 @@ public class BottledEntropy extends BasePotion {
             e.printStackTrace();
         } */
 
-        int targetSlot = this.slot;  // The slot of the current potion
-
-        // Clear the slot manually
-        if (p.potions.get(targetSlot).isObtained) {
-            p.potions.set(targetSlot, new PotionSlot(targetSlot));
-            p.potions.get(targetSlot).isObtained = false;
-        }
-
-        try {
-            AbstractPotion entropic = new EntropicBrew();
-            p.obtainPotion(targetSlot, entropic);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         // nerf applied, this is the old code to fill all slots
         /*addToBot(new AbstractGameAction() {
