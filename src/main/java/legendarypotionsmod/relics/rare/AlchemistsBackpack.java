3a503aacc45aad5e3a82caea90bb8c7ad0a163a1
JordanRSimons <jords314@gmail.com>
2025-07-01 00:35:20 -0500

    Explosive Powder and Alchemist's Backpack added.

---
+++ src/main/java/legendarypotionsmod/relics/rare/AlchemistsBackpack.java
@@ -0,0 +1,44 @@
+package legendarypotionsmod.relics.rare;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import legendarypotionsmod.relics.BaseRelic;
+
+import static legendarypotionsmod.legendarypotions.makeID;
+
+public class AlchemistsBackpack extends BaseRelic {
+    private static final String NAME = "AlchemistsBackpack"; //The name will be used for determining the image file as well as the ID.
+    public static final String ID = makeID(NAME); //This adds the mod's prefix to the relic ID, resulting in modID:MyRelic
+    private static final AbstractRelic.RelicTier RARITY = RelicTier.RARE; //The relic's rarity.
+    private static final AbstractRelic.LandingSound SOUND = AbstractRelic.LandingSound.CLINK; //The sound played when the relic is clicked.
+
+    public AlchemistsBackpack() {
+        super(ID, NAME, RARITY, SOUND);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+    private boolean triggeredThisCombat = false;
+
+    @Override
+    public void atBattleStart() {
+        triggeredThisCombat = false;
+    }
+
+    @Override
+    public void onUsePotion() {
+        if (!triggeredThisCombat && AbstractDungeon.getCurrRoom().phase == com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase.COMBAT) {
+            triggeredThisCombat = true;
+            flash();
+            addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+            addToBot(new GainEnergyAction(1));
+            addToBot(new DrawCardAction(1));
+        }
+    }
+}
---
+++ src/main/java/legendarypotionsmod/relics/uncommon/ExplosivePowder.java
@@ -0,0 +1,40 @@
+package legendarypotionsmod.relics.uncommon;
+
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import legendarypotionsmod.relics.BaseRelic;
+
+import static legendarypotionsmod.legendarypotions.makeID;
+
+public class ExplosivePowder extends BaseRelic {
+    private static final String NAME = "ExplosivePowder"; //The name will be used for determining the image file as well as the ID.
+    public static final String ID = makeID(NAME); //This adds the mod's prefix to the relic ID, resulting in modID:MyRelic
+    private static final AbstractRelic.RelicTier RARITY = RelicTier.UNCOMMON; //The relic's rarity.
+    private static final AbstractRelic.LandingSound SOUND = AbstractRelic.LandingSound.CLINK; //The sound played when the relic is clicked.
+
+    public ExplosivePowder() {
+        super(ID, NAME, RARITY, SOUND);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+    @Override
+    public void onUsePotion() {
+        if (AbstractDungeon.getCurrRoom().phase == com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase.COMBAT) {
+            flash();
+            addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+            addToBot(new DamageAllEnemiesAction(
+                    null,
+                    DamageInfo.createDamageMatrix(10, true),
+                    DamageInfo.DamageType.THORNS,
+                    com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect.FIRE
+            ));
+        }
+    }
+}
