17267f5fac1f20df6be98522222ba433dd162b1c
JordanRSimons <jords314@gmail.com>
2025-07-11 13:50:32 -0500

    Mysterious Jar -> Witch's Trunk art

--- src/main/java/legendarypotionsmod/relics/common/WitchsTrunk.java
+++ src/main/java/legendarypotionsmod/relics/common/WitchsTrunk.java
@@ -8,13 +8,13 @@ import legendarypotionsmod.relics.BaseRelic;
 
 import static legendarypotionsmod.legendarypotions.makeID;
 
-public class GivePotionC extends BaseRelic {
-    private static final String NAME = "GivePotionC"; //The name will be used for determining the image file as well as the ID.
+public class WitchsTrunk extends BaseRelic {
+    private static final String NAME = "WitchsTrunk"; //The name will be used for determining the image file as well as the ID.
     public static final String ID = makeID(NAME); //This adds the mod's prefix to the relic ID, resulting in modID:MyRelic
     private static final AbstractRelic.RelicTier RARITY = AbstractRelic.RelicTier.COMMON; //The relic's rarity.
     private static final AbstractRelic.LandingSound SOUND = AbstractRelic.LandingSound.CLINK; //The sound played when the relic is clicked.
 
-    public GivePotionC() {
+    public WitchsTrunk() {
         super(ID, NAME, RARITY, SOUND);
     }
 
