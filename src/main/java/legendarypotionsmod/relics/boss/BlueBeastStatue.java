d259a229f214e8266e68e5940f79ab5587c18449
JordanRSimons <jords314@gmail.com>
2025-07-11 00:16:31 -0500

    Orange -> Blue Beast Statue art

--- src/main/java/legendarypotionsmod/relics/boss/BlueBeastStatue.java
+++ src/main/java/legendarypotionsmod/relics/boss/BlueBeastStatue.java
@@ -12,13 +12,13 @@ import java.util.ArrayList;
 
 import static legendarypotionsmod.legendarypotions.makeID;
 
-public class OrangeBeastStatue extends BaseRelic {
-    private static final String NAME = "OrangeBeastStatue"; //The name will be used for determining the image file as well as the ID.
+public class BlueBeastStatue extends BaseRelic {
+    private static final String NAME = "BlueBeastStatue"; //The name will be used for determining the image file as well as the ID.
     public static final String ID = makeID(NAME); //This adds the mod's prefix to the relic ID, resulting in modID:MyRelic
     private static final AbstractRelic.RelicTier RARITY = AbstractRelic.RelicTier.BOSS; //The relic's rarity.
     private static final AbstractRelic.LandingSound SOUND = AbstractRelic.LandingSound.CLINK; //The sound played when the relic is clicked.
 
-    public OrangeBeastStatue() {
+    public BlueBeastStatue() {
         super(ID, NAME, RARITY, SOUND);
     }
 
